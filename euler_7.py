1956e40c772b36f5ab42de033d0b5126ce4497c3
eugenekang <eugene.yc.kang@gmail.com>
2020-08-09 13:10:56 -0400

    Create euler_7.py

---
+++ euler_7.py
@@ -0,0 +1,20 @@
+from euler_3 import is_prime
+
+"""
+What is the 10 001st prime number?
+
+NOT DONE
+"""
+
+def count_to_prime(top):
+    y = 0 # Counter
+    x = 0 # Counter for number of primes.
+    while x < top - 1:
+        y += 2
+        if is_prime(y) == True:
+            x += 1
+    return y
+
+if __name__ == "__main__":
+    print(count_to_prime(3))
+    print("done")
