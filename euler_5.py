4a14dd386029347bbeb08108a488db980a463124
eugenekang <eugene.yc.kang@gmail.com>
2020-08-09 13:10:23 -0400

    Create euler_5.py

---
+++ euler_5.py
@@ -0,0 +1,24 @@
+"""
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+NOT DONE
+"""
+def evenly_divisible(num):
+    for i in range (1, 21):
+        if num % i != 0:
+            return False
+    else: 
+        return True
+
+def solve():
+    var = False
+    i = 1
+    while var == False:
+        var = evenly_divisible(i)
+        ++i
+    return i
+
+if __name__ == "__main__":
+    print(evenly_divisible(250822656000))
