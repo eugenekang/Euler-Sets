580b66ae83a785d05d88375b2c78b74573aa60cd
eugenekang <eugene.yc.kang@gmail.com>
2020-08-11 01:10:52 -0400

    Completed problem 8, started problem 9.

---
+++ euler_9.py
@@ -0,0 +1,16 @@
+import math
+
+"""
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+"""
+"""
+a**2 + b**2 = c**2
+a<b<c
+a+b+c = 1000
+Determine product of all 3
+"""
+target_value = 1000
+ 
+if __name__ == "__main__":
+    print(math.sqrt(19))
